376c8026b0fccaf1f0355a75135eb9e0f7199bc7
Euan <ew154461@renishaw.com>
2024-11-04 14:37:42 +0000

    Completed app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
-const Restaurant = require("../models/index")
+const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
-
-
-
+// Create your GET Request Route Below
+app.get("/restaurants", async (req, res) => {
+  try {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching restaurants" });
+  }
+});
 
 module.exports = app;
